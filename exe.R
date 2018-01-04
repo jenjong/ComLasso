8e49573e57d21aed8a54afdeae948ea5049b9de3
jeon jong-june <jj.jeon@gmail.com>
2018-01-04 17:07:50 -0500

    test
    test

--- exe.R
+++ exe.R
@@ -2,13 +2,13 @@ rm(list = ls())
 library(Matrix)
 source("D:/Jeon/rcode/ComLasso/library/ComLassoR.R")
 n = 100
-pk = rep(c(3,3,4),100)
+pk = rep(c(3,3,4),5000)
 p = sum(pk)
-set.seed(2)
+set.seed(3)
 X = matrix(rnorm(n*p), n, p)
 y = rnorm(n)
 lam_min=0
 tol=1e-08
 system.time({fit <- comLasso(X,y,pk,lam_min,tol=1e-8)})
-fit$coefMat[,1]
-fit$lambda_vec
+#fit$coefMat[,1]
+#fit$lambda_vec
